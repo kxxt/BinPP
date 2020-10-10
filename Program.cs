a2a76333039721ccd2b8d67f5e2e8acf309f2d8a
kxxt <18085551+kxxt@users.noreply.github.com>
2020-10-10 19:22:28 +0800

    Refactoring. Replacing NSFW Project name. Bug Fixes.

--- Program.cs
+++ Program.cs
@@ -8,23 +8,137 @@
  *        USE IT AT YOUR OWN RISK !             *
  ***********************************************/
 using System;
-using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
-namespace SbC51CaoDanBinaryToHex
+namespace BinPP
 {
-	class Program
+	public static class Program
 	{
+		private struct Range
+		{
+			public int start, end;
+			public Range(int st,int ed)
+			{
+				start = st;end = ed;
+			}
+		}
+		private static bool debug=true;
+		private const string StringLiteralMatchRegex = @"
+(               # Capturing group for the string
+    @""               # verbatim string - match literal at-sign and a quote
+    (?:
+        [^""]|""""    # match a non-quote character, or two quotes
+    )*                # zero times or more
+    ""                #literal quote
+|               #OR - regular string
+    ""              # string literal - opening quote
+    (?:
+        \\.         # match an escaped character,
+        |[^\\""]    # or a character that isn't a quote or a backslash
+    )*              # a few times
+    ""              # string literal - closing quote
+)";
+		private static Regex strlmregex = new Regex(StringLiteralMatchRegex,RegexOptions.IgnorePatternWhitespace);
+		/// <summary>
+		/// https://stackoverflow.com/questions/4953737/regex-for-matching-c-sharp-string-literals
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static MatchCollection MatchStringLiteral(string source)
+		{
+			return Regex.Matches(source, StringLiteralMatchRegex,
+										RegexOptions.IgnorePatternWhitespace);
+		}
+		public static string ReplaceBinWithHex(this string source, int len)
+		{
+			var listr = new List<Range>();	
+			string ret = source;
+			Regex regex = new Regex($"0[Bb][0-1]{{{len}}}");
+			Match x = regex.Match(ret);
+			while (x != Match.Empty)
+			{
+				listr.Clear();
+				var mc = MatchStringLiteral(ret);
+				foreach (Match xx in mc)
+				{
+					listr.Add(new Range(xx.Index, xx.Index + xx.Value.Length - 1));
+				}
+				string s = x.Value;
+				int st = x.Index, ed = x.Index + x.Length - 1;
+				var instr = false;
+				foreach (var range in listr)
+					if (range.start <= st && ed <= range.end)
+					{
+						instr = true;
+						break;
+					}
+				if (instr) {
+					x = x.NextMatch();
+					continue;
+				}
+				var bin = s.Substring(2);
+				uint val = Convert.ToUInt32(bin, 2);
+				string hex = "0x" + val.ToString($"x{GetHexLength(len)}");
+				string begin = ret.Substring(0, x.Index);
+				string end = ret.Substring(x.Index + x.Length);
+				ret = begin + hex + end;
+				x = regex.Match(ret);
+			}
+			return ret;
+		}
+		private static int GetHexLength(int len)
+		{
+			return len switch
+			{
+				<=4=>1,
+				>4 and <=8=>2,
+				>8 and <=16=>4,
+				>16 and <=32=>8,
+				_=>throw new NotImplementedException("Not Implemented!")
+			};
+		}
+		private static void RestoreCurrentDir()
+		{
+			RestoreDir(Environment.CurrentDirectory);
+		}
+		private static void RestoreDir(string dir)
+		{
+			DirectoryInfo di = new DirectoryInfo(dir);
+			foreach (var x in di.GetFiles())
+			{
+				if (x.Name.EndsWith(".c") || x.Name.EndsWith(".h"))
+				{
+					var str = File.ReadAllText(x.FullName);
+					if (str.StartsWith("//BINPP\r\n") && str.EndsWith("\r\n//BINPP"))
+						x.Delete();
+				}
+			}
+			foreach (var x in di.GetFiles())
+			{
+				if (x.Name.EndsWith(".binpp"))
+				{
+					var p = x.FullName.Remove(x.FullName.Length - 6);
+					Console.WriteLine(p);
+					x.MoveTo(p);
+				}
+			}
+		}
+		private static void RestoreFile(string file)
+		{
+
+		}
 		public static int Main(string[] args)
 		{
 			try
 			{
 				return Exec(args);
 			}
-			catch
+			catch(Exception e)
 			{
-				Console.WriteLine("Fatal Error in BinPP !");
+				Console.WriteLine("Fatal Error in BinPP ! Possibily permission denied.");
+				Console.WriteLine($"Technical Details :\n===== Stack Trace =====\n{e.StackTrace}\n===== Error Message =====\n{e.Message}");
 				return -1;
 			}
 		}
@@ -32,75 +146,59 @@ namespace SbC51CaoDanBinaryToHex
 		{
 			if (args.Length == 0)
 			{
-				DirectoryInfo di = new DirectoryInfo(Environment.CurrentDirectory);
-				foreach (var x in di.GetFiles())
-				{
-					if (x.Name.EndsWith(".c") || x.Name.EndsWith(".h"))
-					{
-						var str = File.ReadAllText(x.FullName);
-						if (str.StartsWith("//BINPP\r\n") && str.EndsWith("\r\n//BINPP"))
-							x.Delete();
-					}
-				}
-				foreach (var x in di.GetFiles())
-				{
-					if (x.Name.EndsWith(".binpp"))
-					{
-						var p = x.FullName.Remove(x.FullName.Length - 6);
-						Console.WriteLine(p);
-						x.MoveTo(p);
-					}
-				}
+				RestoreCurrentDir();
 				return 0;
 			}
-
 			else
 			{
+				if (args[0] == "restore")
+				{
+					if (args.Length > 1)
+					{
+						foreach(var entry in args)if(entry!="restore")
+						{
+							Restore(entry);
+						}
+					}
+					else RestoreCurrentDir();
+					return 0;
+				}
 				foreach (var t in args)
 				{
-					string file = File.ReadAllText(t);
-					string raw = file;
-					var nf = t + ".binpp";
-					File.WriteAllText(nf, raw);
-					Regex regex8 = new Regex("0[Bb][0-1]{8}");
-					Regex regex16 = new Regex("0[Bb][0-1]{16}");
-					Regex regex32 = new Regex("0[Bb][0-1]{32}");
-
-					var ma32 = regex32.Matches(file);
-					foreach (Match x in ma32)
+					try
 					{
-						string s = x.Value;
-						var bin = s.Substring(2);
-						uint val = Convert.ToUInt32(bin, 2);
-						string hex = "0x" + val.ToString("x8");
-						file = file.Replace(s, hex);
+						Console.WriteLine($"Processing {t} .");
+						string file = File.ReadAllText(t);
+						string raw = file;
+						var nf = t + ".binpp";
+						File.WriteAllText(nf, raw);
+						var mc = MatchStringLiteral(file);
+						file = file
+							.ReplaceBinWithHex(32)
+							.ReplaceBinWithHex(16)
+							.ReplaceBinWithHex(8)
+							.ReplaceBinWithHex(7)
+							.ReplaceBinWithHex(6)
+							.ReplaceBinWithHex(5)
+							.ReplaceBinWithHex(4)
+							.ReplaceBinWithHex(3)
+							.ReplaceBinWithHex(2);
+						File.WriteAllText(t, "//BINPP\r\n" + file + "\r\n//BINPP");
 					}
-
-					var ma16 = regex16.Matches(file);
-					foreach (Match x in ma16)
+					catch
 					{
-						string s = x.Value;
-						var bin = s.Substring(2);
-						uint val = Convert.ToUInt32(bin, 2);
-						string hex = "0x" + val.ToString("x4");
-						file = file.Replace(s, hex);
+						Console.WriteLine($"ERROR : Error occurred while processing file {t} , Skip it .");
+						if(debug)throw;
 					}
-					var ma8 = regex8.Matches(file);
-					foreach (Match x in ma8)
-					{
-						string s = x.Value;
-						var bin = s.Substring(2);
-						uint val = Convert.ToUInt32(bin, 2);
-
-						string hex = "0x" + val.ToString("x2");
-						file = file.Replace(s, hex);
-					}
-
-					File.WriteAllText(t, "//BINPP\r\n" + file + "\r\n//BINPP");
-
 				}
+				Console.WriteLine("All done!");
 				return 0;
 			}
+		}
+
+		private static void Restore(string entry)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
